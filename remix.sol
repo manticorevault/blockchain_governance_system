62117d497315bac72150d087e6e2b8c0c03f1b95
manticorevault <artur_seilah@hotmail.com>
2020-12-01 12:49:27 +0000

    Added the Request struct to the Smart Contract and the contribute function

--- remix.sol
+++ remix.sol
@@ -1,15 +1,38 @@
 pragma solidity ^0.4.17;
 
 contract Project {
+    
+    // Request struct with all request's info
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        
+        
+    }
+    
     address public manager;
+    address[] public voters;
     uint public minimumAmount;
     
-    // Define - as argument - a minimum amount one must pay
-    // in order to participate as a contributor
+    
+    
+    // Define - as props - a minimum amount one must pay
+    // in order to participate as a votingContributor
     function Project(uint minimum) public {
         // Define the manager as the one calling the contract
         manager = msg.sender;
         
         minimumAmount = minimum;
     }
+    
+    function contribute() payable public {
+        // Requires a payment > minimumAmount
+        require(msg.value > minimumAmount); 
+        
+        // Add the payer to the voters array
+        voters.push(msg.sender);
+    }
+    
 }
