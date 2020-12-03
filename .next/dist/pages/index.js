2aac06e94cbb2f473b412c6f3fa414b46f005723
manticorevault <artur_seilah@hotmail.com>
2020-12-03 14:53:28 +0000

    Fixed some bugs on displaying the contract address in the front end

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "/home/artur/Documents/blockchain_governance_system/pages/index.js?entry";
+
+exports.default = function () {
+    return _react2.default.createElement("h1", {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 5
+        }
+    }, " This is the LANDING page ");
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIlJlYWN0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPLEFBRVA7Ozs7Ozs7O2tCQUFlLFlBQU0sQUFDakI7MkJBQ0ksY0FBQTs7c0JBQUE7d0JBQUE7QUFBQTtBQUFBLEtBQUEsRUFESixBQUNJLEFBRVA7QUFKRCIsImZpbGUiOiJpbmRleC5qcz9lbnRyeSIsInNvdXJjZVJvb3QiOiIvaG9tZS9hcnR1ci9Eb2N1bWVudHMvYmxvY2tjaGFpbl9nb3Zlcm5hbmNlX3N5c3RlbSJ9
